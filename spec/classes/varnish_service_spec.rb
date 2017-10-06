099b0f9483b58c7ec8c76f261deb2fc34dd540c7
Emerson Prado <emerson.prado@dataprev.gov.br>
2017-10-06 17:22:08 -0300

    Add missing facts in varnish_service_spec

--- spec/classes/varnish_service_spec.rb
+++ spec/classes/varnish_service_spec.rb
@@ -4,41 +4,48 @@ describe 'varnish::service', :type => :class do
   context "on a Debian OS family" do
     let :facts do
       {
-        :osfamily        => 'Debian',
-        :lsbdistid       => 'Debian',
-        :operatingsystem => 'Debian',
-        :lsbdistcodename => 'foo'
+        :osfamily                  => 'Debian',
+        :lsbdistid                 => 'Debian',
+        :operatingsystem           => 'Debian',
+        :operatingsystemmajrelease => '7',
+        :lsbdistcodename           => 'foo',
+        :init_system               => 'init',
+        :architecture              => 'x86_64',
       }
     end
 
     it { should compile }
     it { should contain_class('varnish::install') }
-      
+
     it { should contain_service('varnish').with(
       'ensure'  => 'running',
       'restart' => '/etc/init.d/varnish reload',
       'require' => 'Package[varnish]'
-      ) 
+      )
     }
   end
 
   context "on a RedHat OS family and sisabled" do
     let :facts do
       {
-        :osfamily => 'RedHat',
+        :osfamily                  => 'RedHat',
+        :operatingsystem           => 'RedHat',
+        :operatingsystemmajrelease => '6',
+        :init_system               => 'init',
+        :architecture              => 'x86_64',
       }
     end
-    
+
     let(:params) { { :start => 'no' } }
 
     it { should compile }
     it { should contain_class('varnish::install') }
-      
+
     it { should contain_service('varnish').with(
       'ensure'  => 'stopped',
       'restart' => '/sbin/service varnish reload',
       'require' => 'Package[varnish]'
-      ) 
+      )
     }
-  end  
+  end
 end
